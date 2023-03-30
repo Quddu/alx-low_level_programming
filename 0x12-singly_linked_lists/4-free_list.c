f7093a26ebea3c38d126c1aa2f7ad3b03d85a355
Quddu <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@github.com>
2023-03-30 05:25:45 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,18 +2,22 @@
 #include "lists.h"
 
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
- */
+  * free_list - Frees a linked list.
+  * @head: The pointer to the first node of linked list
+  *
+  * Return: Nothing
+  */
 void free_list(list_t *head)
 {
- list_t *temp;
+	list_t *temp;
 
- while (head)
- {
- temp = head->next;
- free(head->str);
- free(head);
- head = temp;
- }
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+
+	free(head);
 }
