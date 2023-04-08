ae025d88adfa53fb56aaf8789c779e89802a8c1e
Quddu <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@github.com>
2023-04-08 16:35:15 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,77 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _atoi - converts a character to an integer
- *
- * @c: char to convert
- *
- * Return: converted integer
- */
-unsigned int _atoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-
-/**
- * _strlen - gets the length of a string
- *
- * @str: string input
- *
- * Return: string length
- */
-unsigned int _strlen(const char *str)
-{
-	unsigned int index = 0;
-
-	while (str[index] != '\0')
-	{
-		index++;
-	}
-	return (index);
-}
-
-/**
- * binary_to_uint - converts a binary number to an unsigned integer
- *
- * @b: string that contains 0 and 1 characters
- *
- * Return: the number converted or 0 if @b contains a character
- *         that is not 0 or 1 or when @b is null
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int index;
-	unsigned int result = 0, base2 = 1, num = 0;
+	unsigned int total, power;
+	int len;
 
-	if (b == NULL) /* Check if b is NULL */
-	{
+	if (b == NULL)
 		return (0);
-	}
 
-#ifdef DEBUG
-	printf("String is %s, and length is %u.\n", b, _strlen(b)); /* Debug statement */
-#endif
-
-	for (index = _strlen(b) - 1; index >= 0; index--)
+	for (len = 0; b[len]; len++)
 	{
-		num = _atoi(b[index]); /* Convert char to number */
-
-#ifdef DEBUG
-		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2); /* Debug statement */
-#endif
-
-		if (num != 0 && num != 1) /* Check if num is not 0 or 1 */
-		{
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		}
-
-		result += num * base2; /* Calculate result */
-		base2 *= 2;
-
-#ifdef DEBUG
-		printf("Result is %u.\n", result); /* Debug statement */
-#endif
 	}
 
-	return (result);
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
 }
